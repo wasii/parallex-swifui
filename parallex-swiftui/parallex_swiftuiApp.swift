c10f48cb31e4e2d0eca065a2bc2aa6c85cf9af36
abdWasiq <khatri.wasiq@gmail.com>
2024-05-29 14:48:50 +0500

    project init

---
+++ parallex-swiftui/parallex_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  parallex_swiftuiApp.swift
+//  parallex-swiftui
+//
+//  Created by Abul Jaleel on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct parallex_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
